ac29be73567ef4b975e95a376b608dff26375211
Julian D <julianderwisz@gmail.com>
2022-01-26 12:48:50 -0500

    Gilbert JS HW 1

---
+++ js/gilbert-js-hw-concanetation/script-tag.js
@@ -0,0 +1,16 @@
+let studentOne = "tom";
+let studentTwo = "jerry";
+let studentThree = "fred";
+
+let semesterGradeOne = 95;
+let semesterGradeTwo = 93;
+let semesterGradeThree = 90;
+
+// tom: 95
+console.log(studentOne + ': ' + semesterGradeOne);
+
+// jerry: 93
+console.log(studentTwo + ': ' + semesterGradeTwo);
+
+// fred: 90
+console.log(studentThree + ': ' + semesterGradeThree);
