0a5eccb2e78ae8db50095fd01731e6be1bba4552
Julian D <julianderwisz@gmail.com>
2022-02-09 13:31:02 -0500

    Gilbert HW

---
+++ js/gilbert-js-interpolation-concatenation-hw/javascripthomework2/interpolation.js
@@ -0,0 +1,22 @@
+/*
+Store each of the three names listed below in a variable, and then interpolate those variables into a string, to console.log() the following sentence:
+
+"My friends group includes John, Adam, and Mary."
+*/
+
+let friend1 = 'John';
+let friend2 = 'Adrien';
+let friend3 = 'Mary';
+
+console.log(`My friends group includes ${friend1}, ${friend2}, and ${friend3}.`);
+
+/*
+Create two string variables: one that stores the word "Fizz", and another that stores the word "Buzz". Then concanete them together, without a space, to console.log() the following text:
+
+"FizzBuzz"
+*/
+
+let coke = "Fizz";
+let sprite = "Buzz";
+let both = coke + sprite;
+console.log(both);
